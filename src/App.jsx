4efbb561246e694eb120faf47d6fd20aea1ee043
unnati kanswal <unnatikanswal@gmail.com>
2024-04-04 23:11:05 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 export default function App() {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function SignIn() {
+  return (
+    <div>
+      SignIn Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+      quidem, ea quis consequuntur culpa non earum quaerat, nulla rem
+      exercitationem blanditiis optio ipsa dolore nihil reprehenderit id dolor,
+      unde iusto?
+    </div>
+  );
+}
+export default SignIn;
