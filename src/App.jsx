517a3e5e6d305078fe7dee745c79ba834064cc6c
unnati kanswal <unnatikanswal@gmail.com>
2024-04-05 00:16:36 +0530

    create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import SignOut from "./pages/SignOut";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
