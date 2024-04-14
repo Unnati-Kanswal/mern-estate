1099e8852a2b96c20e556ee3f7853ff0fc7355c3
unnati kanswal <unnatikanswal@gmail.com>
2024-04-14 21:11:10 +0530

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
@@ -14,7 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
       </Routes>
